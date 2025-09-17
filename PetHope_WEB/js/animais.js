dd325eafbcd80a59bb85817e364811b4623fa6fd
Maria Eduarda <maria.ferreira22027@gmail.com>
2025-09-17 18:02:09 -0300

    Código atualizado

---
+++ PetHope_WEB/js/animais.js
@@ -0,0 +1,44 @@
+const animais = [
+  {
+    id: 1,
+    nome: "Miau",
+    tipo: "Gato",
+    idade: "2 anos",
+    imagem: "img/gato1.jpg",
+    doador: "Sim",
+    tratamentos: "Vacinado e vermifugado",
+    observacoes: "Carinhoso, gosta de brincar com bolinhas",
+    responsavel: {
+      email: "responsavel1@exemplo.com",
+      telefone: "(11) 98888-1111"
+    }
+  },
+  {
+    id: 2,
+    nome: "Rex",
+    tipo: "Cachorro",
+    idade: "3 anos",
+    imagem: "img/cachorro1.jpg",
+    doador: "Não",
+    tratamentos: "Castrado e vacinado",
+    observacoes: "Muito ativo, precisa de espaço para correr",
+    responsavel: {
+      email: "responsavel2@exemplo.com",
+      telefone: "(11) 97777-2222"
+    }
+  },
+  {
+    id: 3,
+    nome: "Bolinha",
+    tipo: "Porquinho-da-índia",
+    idade: "1 ano",
+    imagem: "img/porquinho1.jpg",
+    doador: "Não",
+    tratamentos: "Nenhum problema de saúde",
+    observacoes: "Adora comer legumes frescos",
+    responsavel: {
+      email: "responsavel3@exemplo.com",
+      telefone: "(11) 96666-3333"
+    }
+  }
+];
